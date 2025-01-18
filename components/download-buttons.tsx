59d6deb71cf0174239da77aa3eb4c632d959a12a
Naitik Jain <naitikjain2005@gmail.com>
2025-01-18 15:52:05 +0530

    FinalFinalFinal

--- components/download-buttons.tsx
+++ components/download-buttons.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Button } from '@/components/ui/button'
+import { Button } from '../components/ui/button'
 
 export function DownloadButtons() {
   const handleDownload = (store: string) => {
